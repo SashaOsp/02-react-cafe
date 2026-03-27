9472ca1acc6cdd75bcd9a6ed6b3e075132c0cd9a
SashaOsp <sasha.mykol@gmail.com>
2026-03-27 09:31:12 +0200

    name fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import css from "./App.module.css";
 import type { Votes, VoteType } from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
-import Notification from "../Notifications/Notification";
+import Notification from "../Notification/Notification";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
